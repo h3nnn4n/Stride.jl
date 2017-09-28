6352d1cc7126248a41df3354c5cd0e163df040c3
h3nnn4n <uber.renan@gmail.com>
2017-09-28 16:41:43 -0300

    Fixed a typo in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ download("https://files.rcsb.org/download/1CRN.pdb", "1crn.pdb")
 data = stride_run("1crn.pdb")
 
 @test data.chain[2].name == "THR"
-@test data.chain[1].ss_type == "C"
-@test data.chain[2].ss_type == "E"
+@test data.chain[1].ss_type == 'C'
+@test data.chain[2].ss_type == 'E'
 @test data.chain[7].ss_type_full == "AlphaHelix"
