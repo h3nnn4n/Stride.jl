b5dd16c384cf74abf19182cb0e3ad6c183f575d1
h3nnn4n <uber.renan@gmail.com>
2017-09-28 15:05:44 -0300

    Working basic Stride

--- src/Stride.jl
+++ src/Stride.jl
@@ -1,12 +1,62 @@
 module Stride
 
-stride_path = "/home/h3nnn4n/Stride.jl/src/tmp/stride"
-input_file = ""
-output_file = ""
-args = [input_file, "-f", output_file]
+export stride_run,
+       stride_update_path,
+       stride_parse
 
-cmd_string = `$stride_path $args`
+type Stride_guts
+    stride_path :: AbstractString
+end
 
-run(cmd_string)
+type SSDataResidue                 # Object to hold information about one residue
+    residue      :: AbstractString # The 3 letter residue name
+    ss_type      :: Char           # The one letter secondary structure
+    ss_type_full :: AbstractString # Full SS name
+    phi          :: Float64        # The phi
+    psi          :: Float64        # and psi dihedral angle
+    area         :: Float64        # Solvent accessible area
+end
+
+type SSData # Holds the data for a chain
+    chain :: Array{SSDataResidue, 1}
+end
+
+stride_guts = Stride_guts("")
+
+function stride_run(in_path :: AbstractString)
+    out_path = randstring(32)
+
+    args = [in_path, "-f$(out_path)"]
+    cmd_string = `$(stride_guts.stride_path) $args`
+    run(cmd_string)
+
+    return stride_parse(out_path)
+end
+
+function stride_update_path(path :: AbstractString)
+    stride_guts.stride_path = path
+end
+
+function stride_parse(path :: AbstractString)
+    data = readdlm(path)
+    nlines, ncolumns = size(data)
+
+    ss = SSData([])
+
+    for i in 1:nlines
+        if data[i] == "REM"
+            continue
+        elseif data[i] == "LOC"
+            continue
+        elseif data[i] == "ASG"
+            push!(ss.chain, SSDataResidue(data[i, 2], data[i, 6][1],
+                                            data[i, 7], data[i, 8],
+                                            data[i, 9], data[i, 10]
+                                            ))
+        end
+    end
+
+    return ss
+end
 
 end
