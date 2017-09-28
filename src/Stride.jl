21bf61b21112f6741b45db2b11f6d99c3cb92f2d
h3nnn4n <uber.renan@gmail.com>
2017-09-28 16:02:42 -0300

    Improved documentation +check if stride is in path

--- src/Stride.jl
+++ src/Stride.jl
@@ -2,7 +2,8 @@ module Stride
 
 export stride_run,
        stride_update_path,
-       stride_parse
+       stride_parse,
+       exists_in_path
 
 
 type Stride_guts
@@ -11,25 +12,25 @@ type Stride_guts
 end
 
 """
-    SSDataResidue holds the information related to the secondary structure
-    associated to a residue.
+SSDataResidue holds the information related to the secondary structure
+associated to a residue.
 
-    The fields are as follows:
-    | Field Name   | Field Type     | Description                        |
-    | ---          | ---            | ---                                |
-    | residue      | AbstractString | The 3 letter residue name          |
-    | ss_type      | Char           | The one letter secondary structure |
-    | ss_type_full | AbstractString | Full SS name                       |
-    | phi          | Float64        | The phi                            |
-    | psi          | Float64        | and psi dihedral angle             |
-    | area         | Float64        | Solvent accessible area            |
+The fields are as follows:
+| Field Name   | Field Type     | Description                        |
+| ---          | ---            | ---                                |
+| residue      | AbstractString | The 3 letter residue name          |
+| ss_type      | Char           | The one letter secondary structure |
+| ss_type_full | AbstractString | Full SS name                       |
+| phi          | Float64        | The phi                            |
+| psi          | Float64        | and psi dihedral angle             |
+| area         | Float64        | Solvent accessible area            |
 
-    The following Secondary Structures are taken into account:
-    | --- | ---         |
-    | H   | Alpha Helix |
-    | E   | Beta Strand |
-    | C   | Coil        |
-    | T   | Turn        |
+The following Secondary Structures are taken into account:
+| --- | ---         |
+| H   | Alpha Helix |
+| E   | Beta Strand |
+| C   | Coil        |
+| T   | Turn        |
 """
 type SSDataResidue
     residue      :: AbstractString
@@ -42,7 +43,7 @@ end
 
 
 """
-    SSData holds the information related to a given protein
+SSData holds the information related to a given protein
 """
 type SSData
     chain :: Array{SSDataResidue, 1}
@@ -51,14 +52,27 @@ end
 stride_guts = Stride_guts("", true)
 
 """
-    stride_run runs Stride for a given pdb file,
-    the return is a SSData object
+    stride_run(in_path :: AbstractString)
+
+Runs Stride for a given pdb file,
+the return is a SSData object
 """
 function stride_run(in_path :: AbstractString)
+    if length(stride_guts.stride_path) == 0
+        if exists_in_path("stride")
+            stride_path = "stride"
+        else
+            error("Stride not found in PATH!\nPlease add stride to PATH" *
+                  " or run stride_update_path(\"pathtostride\")")
+        end
+    else
+        stride_path = stride_guts.stride_path
+    end
+
     out_path = randstring(32)
 
     args = [in_path, "-f$(out_path)"]
-    cmd_string = `$(stride_guts.stride_path) $args`
+    cmd_string = `$(stride_path) $args`
     run(cmd_string)
 
     ss = stride_parse(out_path)
@@ -71,16 +85,20 @@ function stride_run(in_path :: AbstractString)
 end
 
 """
-    stride_update_path can be used to change the path where the package will
-    look for the Stride binary.
+    stride_update_path(path :: AbstractString)
+
+stride_update_path can be used to change the path where the package will
+look for the Stride binary.
 """
 function stride_update_path(path :: AbstractString)
     stride_guts.stride_path = path
 end
 
 """
-    stride_delete_tmp_files recieves a boolean value that determines if the tempfiles
-    will be deleted after use. Default option is to delete it.
+    stride_delete_tmp_files(delete :: Bool)
+
+stride_delete_tmp_files recieves a boolean value that determines if the tempfiles
+will be deleted after use. Default option is to delete it.
 """
 function stride_delete_tmp_files(delete :: Bool)
     stride_guts.delete_tmp_files = delete
@@ -110,5 +128,35 @@ function stride_parse(path :: AbstractString)
 
     return ss
 end
+
+"""
+    exists_in_path(prog::String)
+
+Determine whether the given program name or path is executable using the current user's
+permissions. This is roughly equivalent to querying `which program` at the command line
+and checking that a result is found, but no shelling out occurs.
+"""
+function exists_in_path(prog::String) # Code taken from Alex Arslan with permision
+    X_OK = 1 << 0 # Taken from unistd.h
+    # If prog has a slash, we know the user wants to determine whether the given
+    # file exists and is executable
+    if '/' in prog
+        isfile(prog) || return false
+        return ccall(:access, Cint, (Ptr{UInt8}, Cint), prog, X_OK) == 0
+    end
+    path = get(ENV, "PATH", "")
+    # Something is definitely wrong if the user's path is empty...
+    @assert !isempty(path)
+    sep = ':'
+    for dir in split(path, sep), file in readdir(dir)
+        if file == prog
+            p = joinpath(dir, file)
+            @assert isfile(p)
+            return ccall(:access, Cint, (Ptr{UInt8}, Cint), p, X_OK) == 0
+        end
+    end
+    false
+end
+isexecutable(prog::String) = isexecutable(Sys.KERNEL, prog)
 
 end
