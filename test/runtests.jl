58a57bd2a18bbec484546e0f21d0d5f7d85a978b
h3nnn4n <uber.renan@gmail.com>
2017-10-06 06:55:24 +0000

    Improved testing a bit

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,21 +3,25 @@ using Base.Test
 
 download("https://files.rcsb.org/download/1CRN.pdb", "1crn.pdb")
 
-data = stride_run("1crn.pdb")
+@testset "Secondary Strucure Assignment" begin
+    data = stride_run("1crn.pdb")
 
-@test data.chain[2].name == "THR"
-@test data.chain[1].ss_type == 'C'
-@test data.chain[2].ss_type == 'E'
-@test data.chain[7].ss_type_full == "AlphaHelix"
+    @test data.chain[2].name == "THR"
+    @test data.chain[1].ss_type == 'C'
+    @test data.chain[2].ss_type == 'E'
+    @test data.chain[7].ss_type_full == "AlphaHelix"
+end
 
-data = stride_run("1crn.pdb", out_path="1crn_result")
+@testset "File handling" begin
+    data = stride_run("1crn.pdb", out_path="1crn_result")
 
-@test isfile("1crn_result")
+    @test isfile("1crn_result")
+
+    if isfile("1crn_result")
+        rm("1crn_result")
+    end
+end
 
 if isfile("1crn.pdb")
     rm("1crn.pdb")
-end
-
-if isfile("1crn_result")
-    rm("1crn_result")
 end
